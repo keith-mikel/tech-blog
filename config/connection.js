43004f7cf24e1d0a85c826432a5e4a1998c04037
keith-mikel <keith.mikel@e5sol.com>
2023-08-15 15:29:44 -0500

    added database

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,18 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  // Use JawsDB URL provided by Heroku
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // Fallback to environment variables for local development
   sequelize = new Sequelize(
-    process.env.DB_NAME,
+    process.env.DATABASE_URL || process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: process.env.DB_HOST || 'localhost', // Use environment variable or default to 'localhost'
       dialect: 'mysql',
-      port: 3306
+      port: process.env.DB_PORT || 3306 // Use environment variable or default to 3306
     }
   );
 }
