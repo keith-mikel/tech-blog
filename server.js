4c5cdd8169538270030f9fc6e3ee388912824997
keith-mikel <keith.mikel@e5sol.com>
2023-08-15 15:56:20 -0500

    true

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(routes);
 
 // Sync the Sequelize models with the database
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
